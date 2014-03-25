fe0dc929b507f057239caa82c82e30c3538226e8
Eric Branson <esbranson@gmail.com>
2014-03-25 15:52:26 -0600

    Add NLTK parser

---
+++ util/aknnltk.py
@@ -0,0 +1,34 @@
+##
+# Corpus reader for Akoma Ntoso documents.
+#
+# import nltk.text, aknnltk
+# t = nltk.text.Text(aknnltk.AKNCorpusReader('/tmp/openlaw-test', '.*\.xml').words('pen.xml'))
+# t.plot(20)
+#
+
+from nltk.tokenize import WordPunctTokenizer
+from nltk.corpus.reader.xmldocs import XMLCorpusReader
+
+class AKNCorpusReader(XMLCorpusReader):
+	"""
+	Corpus reader for Akoma Ntoso documents.
+
+	"""
+	def __init__(self, root, fileids):
+		XMLCorpusReader.__init__(self, root, fileids)
+	
+	def xml(self, fileid=None):
+		return XMLCorpusReader.xml(self, fileid)
+	
+	def words(self, fileid=None):
+		"""
+		Returns all of the words and puncuation symbols in the specified file
+		that were in 'section//p' text nodes.
+		"""
+		elt = self.xml(fileid).iterfind('.//section//p')
+		word_tokenizer = WordPunctTokenizer()
+		return [val for subl in [word_tokenizer.tokenize(nodetext) for nodetext in [''.join(el.itertext()) for el in elt]] for val in subl]
+	
+	def raw(self, fileids=None):
+		return XMLCorpusReader.raw(self, fileids)
+
