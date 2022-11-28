5976a6cab59ee28e9da189991c729c5bfed33883
Eric Branson <esbranson@gmail.com>
2022-11-28 11:36:37 -0700

    PS: Organize into classes

--- util/AkomaNtoso.psm1
+++ util/AkomaNtoso.psm1
@@ -28,13 +28,18 @@ function Import-AkomaNtoso {
     )
 
     Process {
-        #if ($null -eq $Document) {return} # TODO Not needed if sanity checks all handle nullable variables and the default is return.
+        #
+        # Parse the user input. Be flexible.
+        #
+
+        if ($null -eq $Document) {return} # TODO Not needed if sanity checks all handle nullable variables and the default is return.
+
         if ($Document -is [xml]) {} # TODO Check that it's Akoma Ntoso.
         elseif ($Document -is [string] -and [System.Uri]::IsWellFormedUriString($Document, [System.UriKind]::Absolute)) {
             $proxy = [System.Net.WebRequest]::GetSystemWebProxy()
             $proxyParameter = @{}
             if ($proxy.GetProxy($Document)) { $proxyParameter = @{Proxy = $proxy.GetProxy($Document); ProxyUseDefaultCredentials = $true } }
-            try { $Document = Invoke-RestMethod $Document @proxyParameter -ErrorAction Stop } catch { throw } # TODO Why is this not terminating without explicitly throwing?
+            $Document = Invoke-RestMethod $Document @proxyParameter # TODO Why is this not terminating without explicitly throwing?
         }
         elseif ($Document -and $Document.GetType() -in @([string], [System.IO.Stream], [System.IO.TextReader], [System.Xml.XmlReader])) {
             $DocumentInput = $Document
@@ -43,82 +48,103 @@ function Import-AkomaNtoso {
         }
         elseif ($Document -is [object[]]) { return ($Document | Import-Akn) }
         else { return } # TODO Respectfully throw.
+        if ($null -eq $Document -or $Document -isnot [System.Xml.XmlDocument]) {return} 
 
-        $nsmgr = New-Object -TypeName System.Xml.XmlNamespaceManager -ArgumentList $Document.NameTable
-        $nsmgr.AddNamespace("akn", "http://docs.oasis-open.org/legaldocml/ns/akn/3.0")
-        $nsmgr.AddNamespace("uslm", "http://schemas.gpo.gov/xml/uslm")
-        $path = $xPathSection
-        if ($IncludeAll) {$path = $xPathSectionAll}
-        $Nodes = $Document.DocumentElement.SelectNodes($path, $nsmgr)
-        Write-Verbose "Nodes.Count $($Nodes.Count)"
-
-        $Nodes.forEach{
-            # TODO Should we sanity check $_ type?
-            $Id = Get-Id $_ $nsmgr
-            $Num = Get-Num $_ $nsmgr
-            $Heading = Get-XmlInnerText $_.SelectSingleNode($xPathHeading, $nsmgr)
-            $IsRepealed = Test-HeadingIsRepealed $heading # TODO
-
-            $_.SelectNodes($xPathRemoveAll, $nsmgr).ForEach{ $_.ParentNode.RemoveChild($_)>$null } # TODO This leaves errant spaces.
-            $Content = Get-XmlInnerText $_.SelectSingleNode($xPathSelf, $nsmgr)
-
-            [PSCustomObject][ordered]@{
-                PSTypeName = "AknSection";
-                Id         = $Id;
-                Num        = $Num;
-                Heading    = $Heading;
-                IsRepealed = $IsRepealed;
-                Content    = $Content;
-            } | 
-            Where-Object { $IncludeAll -or -not $_.IsRepealed }
-        }
-    }
-
-    Begin {
-        $xPathSection = "//akn:section[not(ancestor::akn:hcontainer)]|//uslm:section[not(ancestor::uslm:quotedContent)]"
-        $xPathSectionAll = "//akn:section|//uslm:section"
-        $xPathUSCIdentifierAttr = "./uslm:editorialNote[@role='uscRef']/uslm:ref/@href"
-        $xPathUSCSidenoteIdentifierAttr = "./uslm:sidenote//uslm:ref/@href" # TODO
-        $xPathNumAttr = "./akn:num|./uslm:num/@value"
-        $xPathHeading = "./akn:heading|./uslm:heading"
-        $xPathSelf = "."
-        $xPathRemoveAll = "./akn:num|./akn:heading|./uslm:num|./uslm:heading|.//uslm:editorialNote[@role='uscRef']|.//*[self::uslm:footnote or self::uslm:sourceCredit or self::uslm:sidenote or self::uslm:page]"
-        $reIsRepealed = '^([. ]+)$' # TODO Process notes.
-
-        function Get-XmlAttributeValue($obj) { if ($obj -is [System.Xml.XmlAttribute]) { $obj.Value } }
-
-        function Get-XmlInnerText([System.Xml.XmlNode]$obj) { if ($obj -and $obj.InnerText) { $obj.InnerText.Trim() } }
-
-        <#
-        .SYNOPSIS
-        Get all identifiers of a node.
-        #>
-        function Get-Id([System.Xml.XmlNode]$Element, [System.Xml.XmlNamespaceManager]$nsmgr) {
-            # USLM USC identifier
-            $UslmUscId = Get-XmlAttributeValue $Element.SelectSingleNode($xPathUSCIdentifierAttr, $nsmgr)
-            if ($UslmUscId) { $UslmUscId }
-            $UslmUscIdSidenote = Get-XmlAttributeValue $Element.SelectSingleNode($xPathUSCSidenoteIdentifierAttr, $nsmgr)
-            if ($UslmUscIdSidenote) { $UslmUscIdSidenote }
-
-            # Amoma Ntoso eID
-            $AknId = $Element.Attributes["eId"]
-            if ($AknId) { $AknId.Value }
-
-            # USLM identifier
-            $UslmId = Get-XmlAttributeValue  $Element.Attributes["identifier"]
-            if ($UslmId) { $UslmId }
-        }
-
-        function Get-Num([System.Xml.XmlNode]$Element, [System.Xml.XmlNamespaceManager]$nsmgr) {
-            $AknNum = Get-XmlInnerText $Element.SelectSingleNode($xPathNumAttr, $nsmgr)
-            if ($AknNum) { return $AknNum }
-            $UslmNum = Get-XmlAttributeValue $_.SelectSingleNode($xPathNumAttr, $nsmgr)
-            if ($UslmNum) { return $UslmNum }
-        }
-
-        function Test-HeadingIsRepealed([string]$heading) { if ($heading -match $reIsRepealed) { $true } else { $false } }
+        ([AkomaNtosoDocument]::new($Document, $IncludeAll)).Sections
     }
 }
-Update-TypeData -TypeName "AknSection" -DefaultDisplayPropertySet "Num", "Heading" -DefaultKeyPropertySet "Num" -ErrorAction SilentlyContinue
+
+class AkomaNtosoSection {
+    [string[]]$Id
+    [string]$Num
+    [string]$Heading
+    [boolean]$IsRepealed
+    [string]$Content
+
+    hidden static [string]$xPathUSCIdentifierAttr = "./uslm:editorialNote[@role='uscRef']/uslm:ref/@href"
+    hidden static [string]$xPathUSCSidenoteIdentifierAttr = "./uslm:sidenote//uslm:ref/@href" # TODO
+    hidden static [string]$xPathNumAttr = "./akn:num|./uslm:num/@value"
+    hidden static [string]$xPathHeading = "./akn:heading|./uslm:heading"
+    hidden static [string]$xPathSelf = "."
+    hidden static [string]$xPathRemoveAll = "./akn:num|./akn:heading|./uslm:num|./uslm:heading|.//uslm:editorialNote[@role='uscRef']|.//*[self::uslm:footnote or self::uslm:sourceCredit or self::uslm:sidenote or self::uslm:page]"
+    hidden static [string]$reIsRepealed = '^([. ]+)$' # TODO Process notes.
+
+    AkomaNtosoSection(
+        [System.Xml.XmlNode]$Xml,
+        [System.Xml.XmlNamespaceManager]$XmlNamespaceManager
+    ){
+        $this.Id = [AkomaNtosoSection]::GetId($Xml, $XmlNamespaceManager)
+        $this.Num = [AkomaNtosoSection]::GetNum($Xml, $XmlNamespaceManager)
+        $this.Heading = [AkomaNtosoSection]::GetXmlInnerText($_.SelectSingleNode([AkomaNtosoSection]::xPathHeading, $XmlNamespaceManager))
+        $this.IsRepealed = [AkomaNtosoSection]::TestHeadingIsRepealed($this.Heading)
+        $this.Content = [AkomaNtosoSection]::GetXmlInnerText($_.SelectSingleNode([AkomaNtosoSection]::xPathSelf, $XmlNamespaceManager))
+    }
+
+    static [string[]]GetId(
+        [System.Xml.XmlNode]$Element,
+        [System.Xml.XmlNamespaceManager]$NamespaceManager
+    ){
+        # Amoma Ntoso eID
+        $AknId = $Element.Attributes["eId"]
+        if ($AknId) {$AknId = $AknId.Value}
+
+        # USLM identifier
+        $UslmId = [AkomaNtosoSection]::GetXmlAttributeValue($Element.Attributes["identifier"])
+
+        # USLM USC identifier
+        $UslmUscId = [AkomaNtosoSection]::GetXmlAttributeValue($Element.SelectSingleNode([AkomaNtosoSection]::xPathUSCIdentifierAttr, $NamespaceManager))
+        $UslmUscId = [AkomaNtosoSection]::GetXmlAttributeValue($Element.SelectSingleNode([AkomaNtosoSection]::xPathUSCSidenoteIdentifierAttr, $NamespaceManager))
+
+        return (@($AknId, $UslmId, $UslmUscId, $UslmUscId) | ? {$_})
+    }
+
+    static [string]GetNum(
+        [System.Xml.XmlNode]$Element,
+        [System.Xml.XmlNamespaceManager]$NamespaceManager
+    ){
+        $AknNum = [AkomaNtosoSection]::GetXmlInnerText($Element.SelectSingleNode([AkomaNtosoSection]::xPathNumAttr, $NamespaceManager))
+        $UslmNum = [AkomaNtosoSection]::GetXmlAttributeValue($Element.SelectSingleNode([AkomaNtosoSection]::xPathNumAttr, $NamespaceManager))
+
+        return ($AknNum ? $AknNum : $UslmNum)
+    }
+
+    static [boolean]TestHeadingIsRepealed([string]$heading){
+        if ($heading -match [AkomaNtosoSection]::reIsRepealed) {return $true}
+        return $false
+    }
+
+    static [string]GetXmlAttributeValue($obj){if ($obj -is [System.Xml.XmlAttribute]) {return $obj.Value}; return $null;}
+    static [string]GetXmlInnerText([System.Xml.XmlNode]$obj){if ($obj -and $obj.InnerText) {return $obj.InnerText.Trim()}; return $null;}
+}
+
+class AkomaNtosoDocument {
+    [AkomaNtosoSection[]]$Sections
+    [int]$Count
+
+    hidden static [string]$xPathSection = "//akn:section[not(ancestor::akn:hcontainer)]|//uslm:section[not(ancestor::uslm:quotedContent)]"
+    hidden static [string]$xPathSectionAll = "//akn:section|//uslm:section"
+
+    AkomaNtosoDocument(
+        [System.Xml.XmlDocument]$XmlDocument,
+        [switch]$IncludeAll
+    ){
+        $path = [AkomaNtosoDocument]::xPathSection
+        if ($IncludeAll) {$path = [AkomaNtosoDocument]::xPathSectionAll}
+
+        $XmlNamespaceManager = New-Object -TypeName System.Xml.XmlNamespaceManager -ArgumentList $XmlDocument.NameTable
+        $XmlNamespaceManager.AddNamespace("akn", "http://docs.oasis-open.org/legaldocml/ns/akn/3.0")
+        $XmlNamespaceManager.AddNamespace("uslm", "http://schemas.gpo.gov/xml/uslm")
+
+        $Nodes = $XmlDocument.DocumentElement.SelectNodes($path, $XmlNamespaceManager)
+        $this.Count = $Nodes.Count
+        $this.Sections = $Nodes.forEach{
+            # TODO Should we sanity check $_ type?
+            [AkomaNtosoSection]::new($_, $XmlNamespaceManager)
+        }
+    }
+}
+
+Update-TypeData -TypeName "AkomaNtosoDocument" -DefaultDisplayPropertySet "Count","Sections" -DefaultKeyPropertySet "Sections" -ErrorAction SilentlyContinue
+Update-TypeData -TypeName "AkomaNtosoSection" -DefaultDisplayPropertySet "Num", "Heading" -DefaultKeyPropertySet "Num" -ErrorAction SilentlyContinue
 New-Alias Import-Akn Import-AkomaNtoso
 Export-ModuleMember -Function * -Alias *
